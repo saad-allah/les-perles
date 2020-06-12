feb07705de23cc3144a9e68a353453f970eeaa5d
saad-allah <saad.allah.sae@gmail.com>
2020-06-12 08:17:34 +0100

    add new config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Les perles`,
+    description: `Les Perles de l'Agdal est une résidence fermée et sécurisée qui vous propose des appartements R+6 R+8 dotés d'une architecture et design moderne pensés pour vous offrir des éspaces de vie à la fois reposants et pratiques`,
+    author: `@astro`,
   },
   plugins: [
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -21,12 +23,121 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#037168`,
+        theme_color: `#037168`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/lesperles-favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+  resolve: "gatsby-plugin-react-svg",
+  options: {
+    rule: {
+      include:`/assets/`,// See below to configure properly
+    }
+  }
+},
+{
+    "resolve": "gatsby-plugin-excerpts",
+    "options": {
+        "sources": {
+            "snippetBlocks": {
+                "type": "htmlQuery",
+                "sourceField": "html",
+                "excerptSelector": ".custom-block.snippet .custom-block-body",
+                "stripSelector": "a",
+                "elementReplacements": [
+                    {
+                        "selector": "h6",
+                        "replaceWith": "strong"
+                    },
+                    {
+                        "selector": "h5",
+                        "replaceWith": "h6"
+                    },
+                    {
+                        "selector": "h4",
+                        "replaceWith": "h5"
+                    },
+                    {
+                        "selector": "h3",
+                        "replaceWith": "h4"
+                    },
+                    {
+                        "selector": "h2",
+                        "replaceWith": "h3"
+                    },
+                ],
+            },
+            "default": {
+                "type": "htmlQuery",
+                "sourceField": "html",
+                "excerptSelector": "html > *",
+                "ignoreSelector": "img, .gatsby-highlight",
+                "stripSelector": "a",
+                "elementReplacements": [
+                    {
+                        "selector": "h6",
+                        "replaceWith": "strong"
+                    },
+                    {
+                        "selector": "h5",
+                        "replaceWith": "h6"
+                    },
+                    {
+                        "selector": "h4",
+                        "replaceWith": "h5"
+                    },
+                    {
+                        "selector": "h3",
+                        "replaceWith": "h4"
+                    },
+                    {
+                        "selector": "h2",
+                        "replaceWith": "h3"
+                    },
+                ],
+                "truncate": {
+                    "length": 10,
+                    "byWords": true,
+                    "ellipsis": "…"
+                },
+            }
+        },
+        "sourceSets": {
+            "markdownHtml": [
+                "snippetBlocks",
+                "default"
+            ]
+        },
+        "excerpts": {
+            "snippet": {
+                "type": "html",
+                "nodeTypeSourceSet": {
+                    "MarkdownRemark": "markdownHtml"
+                }
+            }
+        }
+    },
+},
+{
+    "resolve": "gatsby-remark-custom-blocks",
+    "options": {
+        "blocks": {
+            "snippet": {
+                "classes": "snippet"
+            },
+        },
+    },
+}, {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      tableOfContents: {
+        heading: null,
+        maxDepth: 6,
+      },
+    },
+  },
     {
       resolve: "gatsby-source-wordpress",
       options: {
@@ -40,17 +151,19 @@ module.exports = {
         verboseOutput: false,
         searchAndReplaceContentUrls: {
           sourceUrl: "https://les-perles.theastro.co",
-          replacementUrl: "https://localhost:8000",
         },
         // Set how many simultaneous requests are sent at once.
         concurrentRequests: 10,
         includedRoutes: [
           "**/categories",
           "**/posts",
+          "**/appartement",
+          "**/galeries",
           "**/pages",
           "**/media",
           "**/tags",
           "**/taxonomies",
+          "**/style",
           "**/users",
           "**/*/*/menus",
           "**/*/*/menu-locations",
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,72 +1,107 @@
-const path= require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
 
-exports.createPages=({actions,graphql})=>{
-    const {createPage}= actions
-    return graphql(`
-        {
-            allWordpressPost(sort:{fields:[date], order:DESC}) {
-                edges {
-                    node {
-                        id
-                        date( formatString: "/YYYY/MM/DD/" )
-                        featured_media {
-                            localFile{
-                                childImageSharp {
-                                    id
-                                }
-                            }
-                        }
-                        slug
-                        title
-                    }
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  return graphql(`
+    {
+      allWordpressPost(sort: { fields: [date], order: DESC }) {
+        edges {
+          node {
+            id
+            date(formatString: "/YYYY/MM/DD/")
+            featured_media {
+              localFile {
+                childImageSharp {
+                  id
                 }
+              }
             }
+            slug
+            title
+          }
         }
-    `).then(result=>{
-        if (result.errors){
-            result.errors.forEach(e => console.error(e.toString()))
-            return Promise.reject(result.errors)
+      }
+      allWordpressWpAppartement {
+        edges {
+          node {
+            id
+            slug
+          }
         }
-  
-        const postTemplate = path.resolve(`./src/templates/post.js`)
-        const allPosts = result.data.allWordpressPost.edges
+      }
 
-        allPosts.forEach(element=>{
-            createPage({
-                path:`/actualites/${element.node.slug}`,
-                component:postTemplate,
-                context:{
-                    id:element.node.id
-                }
-            })
-        })
-    })
-}
+      allWordpressWpGaleries {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) {
+      result.errors.forEach((e) => console.error(e.toString()));
+      return Promise.reject(result.errors);
+    }
 
+    const postTemplate = path.resolve(`./src/templates/post.js`);
+    const allPosts = result.data.allWordpressPost.edges;
+    const appTemplate = path.resolve(`./src/templates/appt.js`);
+    const allApps = result.data.allWordpressWpAppartement.edges;
+    const galerieTemplate = path.resolve(`./src/templates/appt.js`);
+    const allGaleries = result.data.allWordpressWpGaleries.edges;
+    allPosts.forEach((element) => {
+      createPage({
+        path: `/actualites/${element.node.slug}`,
+        component: postTemplate,
+        context: {
+          id: element.node.id,
+        },
+      });
+    });
+    allApps.forEach((element) => {
+      createPage({
+        path: `/appartements/${element.node.slug}`,
+        component: appTemplate,
+        context: {
+          id: element.node.id,
+        },
+      });
+    });
 
+    allGaleries.forEach((element) => {
+      createPage({
+        path: `/galerie/${element.node.slug}`,
+        component: galerieTemplate,
+        context: {
+          id: element.node.id,
+        },
+      });
+    });
+  });
+};
 
-exports.createResolvers = async (
-  {
-    actions,
-    cache,
-    createNodeId,
-    createResolvers,
-    store,
-    reporter,
-  },
-) => {
-  const { createNode } = actions
+exports.createResolvers = async ({
+  actions,
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  const { createNode } = actions;
 
   await createResolvers({
     WPGraphQL_MediaItem: {
       imageFile: {
         type: "File",
         async resolve(source) {
-          let sourceUrl = source.sourceUrl
+          let sourceUrl = source.sourceUrl;
 
           if (source.mediaItemUrl !== undefined) {
-            sourceUrl = source.mediaItemUrl
+            sourceUrl = source.mediaItemUrl;
           }
 
           return await createFilePath({
@@ -76,9 +111,9 @@ exports.createResolvers = async (
             createNode,
             createNodeId,
             reporter,
-          })
+          });
         },
       },
     },
-  })
-}
+  });
+};
