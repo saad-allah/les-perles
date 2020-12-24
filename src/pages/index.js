b53570b236bd7c921e9b96caf39e3f39414874ec
saad-allah <saad.allah.sae@gmail.com>
2020-12-24 12:42:15 +0100

    lp

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+      <SEO title="Les perles " />
+    <img src="https://admin.lesperles.ma/wp-content/uploads/2020/06/lesperles-logo.svg" alt="Les perles" style={{ width: `200px`}}/>
+
+  </Layout>
+)
+
+export default IndexPage
